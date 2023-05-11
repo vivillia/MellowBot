4e6e3569b6d6689420f0fc274b037011e8d8863d
vivillia <vivianluo1119@hotmail.com>
2023-05-11 00:36:04 -0400

    add anniversary command

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("i love hubby")
+import discord
+from discord.ext import commands
+import json
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def anniversary(ctx):
+    await ctx.send('march 28th, 2022')
+
+with open('credentials.json', 'r') as file:
+    credentials = json.load(file)
+
+mellowBotToken = credentials["token"]
+
+bot.run(mellowBotToken)

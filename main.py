c4ccce69699616a20d3de7f2d237f08d512708fb
vivillia <vivianluo1119@hotmail.com>
2023-06-07 00:58:36 -0400

    MellowBot meets Docker Whale

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import discord
 from discord.ext import commands
 import json
+import gspread
+from dotenv import load_dotenv
+import os
+
+env_vars = load_dotenv('.env')
+
+gc = gspread.service_account(filename='credentials.json')
+sh = gc.open_by_key("18y3BVsmo65efd4qFI4FkM9WgW0TFep4mMD7ExsB9Q1E")
+#setting up google spreadsheet
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
 async def ping(ctx):
@@ -14,9 +23,13 @@ async def ping(ctx):
 async def anniversary(ctx):
     await ctx.send('march 28th, 2022')
 
+@bot.command()
+async def ImportantDates(ctx):
+    await ctx.send('march 28th, 2022')
+
 with open('credentials.json', 'r') as file:
     credentials = json.load(file)
 
-mellowBotToken = credentials["token"]
+mellowBotToken = os.environ.get('token')
 
 bot.run(mellowBotToken)
